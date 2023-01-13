cc7718e5ca0eba6bb31ad740e52cf3dbc13951fb
João Pedro <joaosvieira04@gmail.com>
2023-01-13 12:19:44 -0300

    Subindo meu CRUD!

--- productController.js
+++ productController.js
@@ -6,11 +6,85 @@ exports.createProduct = async (request, response) => {
 
     response.status(201).json({
       data: {
-        product: newProduct
+        Product: newProduct
+      }
+    })
+  } catch (err) {
+    response.status(400).json({
+      message: `The product wasn't created, please type the right parameters`
+    })
+  }
+}
+
+exports.getProduct = async (request, response) => {
+  try {
+    const id = request.params.id
+    const product = await Product.findById(id)
+
+    response.status(200).json({
+      data: {
+        Product: product
+      }
+    })
+  } catch (err) {
+    response.status(404).send('Product not Found!, please type the right id')
+  }
+}
+
+exports.getAllProducts = async (request, response) => {
+  try {
+    const products = await Product.find()
+
+    response.status(200).json({
+      results: products.length,
+      data: {
+        products
+      }
+    })
+  } catch (err) {
+    response.status(404).json({
+      message: err
+    })
+  }
+}
+
+exports.updateProduct = async (request, response) => {
+  try {
+    const newProduct = await Product.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      {
+        new: true,
+        runValidators: true
+      }
+    )
+    response.status(200).json({
+      message: 'Update completed!, this is the product now',
+      data: {
+        Product: newProduct
+      }
+    })
+  } catch (err) {
+    response.status(400).json({
+      message: err
+    })
+  }
+}
+
+exports.deleteProduct = async (request, response) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(request.params.id)
+
+    if (deletedProduct === null) {
+      response.status(404).send('Id not found!')
+    }
+    response.status(202).json({
+      message: "Successful Delete!, that's what has been deleted",
+      data: {
+        Product: deletedProduct
       }
     })
   } catch (err) {
     console.log(err)
-    response.send('vai se ferrar')
   }
 }
